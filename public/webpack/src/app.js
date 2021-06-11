5383d3d7e627a7fbcc09cda06c151c80db2a30a7
A.Sudo <50855593+elkulo@users.noreply.github.com>
2021-06-11 21:40:24 +0900

    カテゴリー実装

--- public/webpack/src/app.js
+++ public/webpack/src/app.js
@@ -9,18 +9,29 @@ const API_PATH = '/api/v1/posts';
 new Vue({
 	el: '#app',
 	data: {
-		isActiveTab: 1,
-		feeds: [],
+		APIQuery: {}, // 元データオブジェクト
+		feederQuery: {}, // フィードオブジェクト
+		categories: [], // カテゴリーリスト
+		isActiveTab: 1, // アクティブな記事ID
+		isActiveCategory: '', // アクティブなカテゴリー名
 		clock: {
-			day: '',
-			time: ''
+			day: '', // 日付
+			time: '' // 時間
 		}
 	},
 	mounted() {
 		this.currentDay();
 		this.currentTime();
 		axios.get( API_PATH ).then( ( response ) => {
-			this.feeds = response.data;
+			this.APIQuery = response.data;
+			this.feederQuery = this.APIQuery.data;
+			let categories = new Array();
+			this.feederQuery.forEach( single => {
+				categories = Array.from(
+					new Set([ ...categories, ...single.category ])
+				);
+			});
+			this.categories = categories;
 		}).catch( ( error ) => {
 			// eslint-disable-next-line no-console
 			console.error( '読み込み失敗', error );
@@ -35,8 +46,31 @@ new Vue({
 			// eslint-disable-next-line no-console
 			console.log( text );
 		},
-		changeTab: function( num ) {
-			this.isActiveTab = num;
+		changeTab: function( tabID ) {
+			this.isActiveTab = tabID;
+		},
+		changeCategory: function( requestCategory = '' ) {
+			let taxonomy = [];
+			let result;
+			this.APIQuery.data.forEach( singleQuery => {
+				result = singleQuery.category.filter( val => val === requestCategory );
+				if ( result[0]) {
+					taxonomy.push( singleQuery );
+				}
+			});
+			if ( taxonomy.length ) {
+
+				// カテゴリーに記事がある場合
+				this.feederQuery = taxonomy;
+				this.isActiveTab = taxonomy[0].id;
+				this.isActiveCategory = requestCategory;
+			} else {
+
+				// 記事がなければ全記事表示
+				this.feederQuery = this.APIQuery.data;
+				this.isActiveTab = 1;
+				this.isActiveCategory = '';
+			}
 		},
 		currentDay: function() {
 			const DaysDate = new Date();
