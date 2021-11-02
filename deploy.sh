9959c5220491c0c87253a3c846299c4cb3ad1e48
A.Sudo <50855593+elkulo@users.noreply.github.com>
2021-11-02 22:06:56 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,14 @@
 # feederディレクトリのZipを作成
 zip -r dist/Feeder-latest-ja.zip feeder/ -x '*.git*' '*.env*' '*.DS_Store' '*__MACOSX*' '*.log*' '*/logs/' '*.sql*' '*/database/'
 
+# .htaccessをhtaccess.txtにリネーム
+mv -n public/.htaccess public/htaccess.txt
+
 # publicディレクトリをZipに追加
-zip -r dist/Feeder-latest-ja.zip public/ -x '*.git*' '*.env*' '*.DS_Store' '*__MACOSX*'
+zip -r dist/Feeder-latest-ja.zip public/ -x '*.git*' '*.env*' '*.DS_Store' '*__MACOSX*' '*.htaccess'
+
+# htaccess.txtを.htaccessにリネーム
+mv -n public/htaccess.txt public/.htaccess
 
 # LICENSEをLICENSE.txtにしてZipに追加
 cp -i LICENSE LICENSE.txt
