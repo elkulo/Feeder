80a38c906f2367a63611d6c74ac01729e5fdb6dd
A.Sudo <50855593+elkulo@users.noreply.github.com>
2022-04-16 14:22:58 +0900

    update

--- public/webpack/src/app.js
+++ public/webpack/src/app.js
@@ -1,36 +1,19 @@
 /*!
- * Feeder | el.kulo v1.0.0 (https://github.com/elkulo/Feeder/)
- * Copyright 2020-2021 A.Sudo
+ * Feeder | el.kulo v2.0.0 (https://github.com/elkulo/Feeder/)
+ * Copyright 2020-2022 A.Sudo
  * Licensed under LGPL-2.1-only (https://github.com/elkulo/Feeder/blob/main/LICENSE)
  */
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Vue from 'vue';
+import { onMounted, onBeforeUnmount, reactive, watch } from 'vue';
 import axios from 'axios';
-import fix100vh from './mixins/fix100vh.js';
-import getCurrentClock from './mixins/getCurrentClock.js';
-import isStorageAvailable from './mixins/isStorageAvailable.js';
-import onBackToTop from './mixins/onBackToTop.js';
-import onScrollEvent from './mixins/onScrollEvent.js';
-import './app.scss';
+import smoothscroll from 'smoothscroll-polyfill';
+import StorageAvailable from './utils/StorageAvailable.js';
+import Fix100vh from './utils/Fix100vh.js';
+import { API_PATH, POSTS_PER_PAGE, RELOAD_TIMER } from './config.js';
 
-// APIのパス.
-const API_PATH = '/api/v1/posts';
-
-// 1ページの表示上限.
-const POSTS_PER_PAGE = 50;
-
-// 自動更新の間隔.
-const RELOAD_TIMER = {
-	id: 0,
-	interval: 15, // リロード間隔(分)
-};
-
-// Vueアプリケーション.
-new Vue({
-	el: '#app',
-	data: function() {
-		return {
+export default {
+	name: 'App',
+	setup() {
+		const state = reactive({
 			APIQuery: [], // APIデータオブジェクト
 			feederQuery: [], // フィードオブジェクト
 			categories: [], // カテゴリーリスト
@@ -54,85 +37,120 @@ new Vue({
 			backToTop: {
 				display: false, // スクロールトップの表示
 			},
-		};
-	},
-	computed: {
+			clock: {
+				day: '', // 日付
+				time: '', // 時間
+			},
+		});
 
-		// 連想配列のパラメーターの変更を取得.
-		computedSetting() {
-			return JSON.parse( JSON.stringify( this.setting ) );
-		},
-	},
-	watch: {
-
-		// APIの更新を監視.
-		APIQuery: {
-			handler: function() {
-				if ( this.setting.autoload ) {
+		watch(
+			() => state.APIQuery,
+			() => {
+				if ( state.setting.autoload ) {
 					clearTimeout( RELOAD_TIMER.id );
-					RELOAD_TIMER.id = setTimeout( () => this.onAjaxReload(), 60 * 1000 * RELOAD_TIMER.interval );
+					RELOAD_TIMER.id = setTimeout( () => onAjaxReload(), 60 * 1000 * RELOAD_TIMER.interval );
 				} else {
 					clearTimeout( RELOAD_TIMER.id );
 				}
 			},
-		},
+			{ deep: true }
+		);
 
-		// 連想配列のパラメーターの変更を比較.
-		computedSetting: {
-			handler: function( newValue, oldValue ) {
+		watch(
+			() => state.setting,
+			( value ) => {
 
 				// オートロード.
-				if ( oldValue.autoload !== newValue.autoload ) {
-					if ( newValue.autoload ) {
-						this.onAjaxReload(); // TrueならAPIを更新.
-					} else {
-						clearTimeout( RELOAD_TIMER.id ); // FalseならTimerを停止.
-					}
+				if ( value.autoload ) {
+					onAjaxReload(); // TrueならAPIを更新.
+				} else {
+					clearTimeout( RELOAD_TIMER.id ); // FalseならTimerを停止.
 				}
 
 				// ダークモード.
-				if ( oldValue.darkmode !== newValue.darkmode ) {
-					if ( newValue.darkmode ) {
-						document.body.classList.add( 'darkmode' );
-					} else {
-						document.body.classList.remove( 'darkmode' );
-					}
+				if ( value.darkmode ) {
+					document.body.classList.add( 'darkmode' );
+				} else {
+					document.body.classList.remove( 'darkmode' );
 				}
 
-				if ( this.isStorageAvailable( 'localStorage' ) ) {
-					localStorage.setItem( 'feeder-app-setting', JSON.stringify( this.setting ) );
+				if ( StorageAvailable.has( 'localStorage' ) ) {
+					localStorage.setItem( 'feeder-app-setting', JSON.stringify( state.setting ) );
 				}
 			},
-			deep: true,
-		},
-	},
-	methods: {
-		onResetSetting: function() {
-			if ( this.isStorageAvailable( 'localStorage' ) ) {
+			{ deep: true }
+		);
+
+		const onCurrentDay = () => {
+			const DaysDate = new Date();
+			const year = DaysDate.getFullYear();
+			const month = ( '0' + ( DaysDate.getMonth() + 1 ) ).slice( -2 );
+			const day = ( '0' + DaysDate.getDate() ).slice( -2 );
+
+			const dayOfWeek = DaysDate.getDay();
+			const dayOfWeekStr = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ][ dayOfWeek ];
+
+			const dayFormat = year + '/' + month + '/' + day + ' <small>' + dayOfWeekStr + '</small>';
+			state.clock.day = dayFormat;
+		};
+
+		const onCurrentTime = () => {
+			let timer = 0;
+
+			const timeLoop = () => {
+				if ( 0 < timer ) {
+					clearTimeout( timer );
+				}
+				const TimesDate = new Date();
+				const hour = ( '0' + TimesDate.getHours() ).slice( -2 );
+				const minute = ( '0' + TimesDate.getMinutes() ).slice( -2 );
+				const second = ( '0' + TimesDate.getSeconds() ).slice( -2 );
+				const timeFormat = hour + ':' + minute + ':<small>' + second + '</small>';
+				state.clock.time = timeFormat;
+
+				timer = setTimeout( () => timeLoop(), 1000 );
+			};
+			timeLoop();
+		};
+
+		const onBackToTop = () => {
+			smoothscroll.polyfill(); // Safari Polyfill.
+			window.scrollTo({
+				behavior: 'smooth',
+				top: 0,
+			});
+		};
+
+		const onResetSetting = () => {
+			if ( StorageAvailable.has( 'localStorage' ) ) {
 				localStorage.removeItem( 'feeder-app-setting' );
-				this.setting = {
+				state.setting = {
 					autoload: true, // 自動更新
 					darkmode: false, // ダークモード
 				};
 			}
-		},
-		toggleSwitchSetting: function( key ) {
-			if ( this.setting[key] === true ) {
-				this.setting[key] = false;
+		};
+
+		const toggleSwitchSetting = ( key ) => {
+			if ( state.setting[key] === true ) {
+				state.setting[key] = false;
 			} else {
-				this.setting[key] = true;
+				state.setting[key] = true;
 			}
-		},
-		changeTab: function( dataSiteID = 0 ) {
-			this.isActiveTab = dataSiteID;
-		},
-		chengeToastDisplay: function( show = false ) {
-			this.toast.display = show;
-		},
-		changeCategory: function( requestCategory = '', dataSiteID = 0 ) {
+		};
+
+		const changeTab = ( dataSiteID = 0 ) => {
+			state.isActiveTab = dataSiteID;
+		};
+
+		const chengeToastDisplay = ( show = false ) => {
+			state.toast.display = show;
+		};
+
+		const changeCategory = ( requestCategory = '', dataSiteID = 0 ) => {
 			let inTaxonomy = [];
 			let result = [];
-			this.APIQuery.data.forEach( ( singleQuery ) => {
+			state.APIQuery.data.forEach( ( singleQuery ) => {
 				result = singleQuery.category.filter( ( val ) => val === requestCategory );
 				if ( result[0]) {
 					inTaxonomy.push( singleQuery );
@@ -142,24 +160,25 @@ new Vue({
 			if ( inTaxonomy.length ) {
 
 				// カテゴリーに記事がある場合
-				this.feederQuery = [ ...this.getQueryAll( requestCategory ), ...inTaxonomy ];
+				state.feederQuery = [ ...getQueryAll( requestCategory ), ...inTaxonomy ];
 			} else {
 
 				// ホームでは全記事表示
-				this.feederQuery = [ ...this.getQueryAll(), ...this.APIQuery.data ];
+				state.feederQuery = [ ...getQueryAll(), ...state.APIQuery.data ];
 			}
-			this.isActiveTab = dataSiteID;
-			this.isActiveCategory = requestCategory;
-		},
-		onAjaxReload: function() {
+			state.isActiveTab = dataSiteID;
+			state.isActiveCategory = requestCategory;
+		};
+
+		const onAjaxReload = () => {
 			let isLoadSuccess = false;
 			axios.get( API_PATH )
 				.then( ( response ) => {
 					isLoadSuccess = true;
 
 					// クエリーを生成
-					this.APIQuery = response.data;
-					this.APIQuery.data.forEach( ( singleQuery ) => {
+					state.APIQuery = response.data;
+					state.APIQuery.data.forEach( ( singleQuery ) => {
 
 						// feeder要素に掲載サイトの情報を追加.
 						singleQuery.feeder.forEach( ( _, index ) => {
@@ -172,38 +191,39 @@ new Vue({
 					});
 
 					// フィードリストを生成
-					this.feederQuery = [ ...this.getQueryAll(), ...this.APIQuery.data ];
+					state.feederQuery = [ ...getQueryAll(), ...state.APIQuery.data ];
 
 					// カテゴリーリストを生成
 					let categories = [];
-					this.APIQuery.data.forEach( ( single ) => {
+					state.APIQuery.data.forEach( ( single ) => {
 						categories = Array.from( new Set([ ...categories, ...single.category ]) );
 					});
-					this.categories = categories;
-					this.changeCategory( this.isActiveCategory, this.isActiveTab );
+					state.categories = categories;
+					changeCategory( state.isActiveCategory, state.isActiveTab );
 
 					// eslint-disable-next-line no-console
-					console.info( 'API loading is complete, at ' + this.clock.time.replace( /(<([^>]+)>)/gi, '' ) );
+					console.info( 'API loading is complete, at ' + state.clock.time.replace( /(<([^>]+)>)/gi, '' ) );
 				})
 				.catch( () => {
 					isLoadSuccess = false;
 
 					// Toastでアラートを表示.
-					this.toast = {
+					state.toast = {
 						display: true,
-						latest: 'at ' + this.clock.time, // 時刻
+						latest: 'at ' + state.clock.time, // 時刻
 						title: 'Failed to load API.', // タイトル
 						content: '最新情報の取得に失敗しました。', // 内容
 					};
 
 					// eslint-disable-next-line no-console
-					console.error( 'Failed to load API. at ' + this.clock.time.replace( /(<([^>]+)>)/gi, '' ) );
+					console.error( 'Failed to load API. at ' + state.clock.time.replace( /(<([^>]+)>)/gi, '' ) );
 				})
 				.finally( () => {
-					this.onAjaxReloadAfter( isLoadSuccess );
+					onAjaxReloadAfter( isLoadSuccess );
 				});
-		},
-		onAjaxReloadAfter: function( isLoadSuccess ) {
+		};
+
+		const onAjaxReloadAfter = ( isLoadSuccess ) => {
 
 			// API読み込みが失敗した場合はスキップ
 			if ( ! isLoadSuccess ) {
@@ -211,34 +231,35 @@ new Vue({
 			}
 
 			// 初回のAPI読み込みか、2回目以降か判定
-			if ( this.hasPreLoad ) {
+			if ( state.hasPreLoad ) {
 
 				// プリロード済みにフラグ変更
-				setTimeout( () => ( this.hasPreLoad = false ), 1000 );
+				setTimeout( () => ( state.hasPreLoad = false ), 1000 );
 
 				// オートロード時間の更新
-				this.autoload = {
+				state.autoload = {
 					display: false,
-					latest: 'Latest ' + this.clock.time,
+					latest: 'Latest ' + state.clock.time,
 				};
 			} else {
 
 				// オートロード時間の更新
-				this.autoload = {
+				state.autoload = {
 					display: true,
-					latest: 'Latest ' + this.clock.time,
+					latest: 'Latest ' + state.clock.time,
 				};
 
 				// オートロードを自動で閉じる
-				setTimeout( () => ( this.autoload.display = false ), 5600 );
+				setTimeout( () => ( state.autoload.display = false ), 5600 );
 			}
-		},
-		getQueryAll: function( requestCategory = '' ) {
+		};
+
+		const getQueryAll = ( requestCategory = '' ) => {
 			let inPosts = [];
 			let result = [];
 
 			// サイトから記事の配列のみを抜き取り結合
-			this.APIQuery.data.forEach( ( singleQuery ) => {
+			state.APIQuery.data.forEach( ( singleQuery ) => {
 				if ( requestCategory ) {
 					result = singleQuery.category.filter( ( val ) => val === requestCategory );
 					if ( result[0]) {
@@ -269,54 +290,59 @@ new Vue({
 					feeder: inPosts.filter( ( val, index ) => index < POSTS_PER_PAGE ),
 				},
 			];
-		},
-		debug: function( text ) {
+		};
+
+		const debug = ( text ) => {
 			// eslint-disable-next-line no-console
 			console.log( text );
-		},
-	},
+		};
 
-	/**
-	 * Vue Created
-	 */
-	created() {
+		onMounted( () => {
+			Fix100vh.add();
+			onCurrentDay();
+			onCurrentTime();
 
-		// ローカルストレージから設定を取得
-		if ( this.isStorageAvailable( 'localStorage' ) ) {
-			const data = localStorage.getItem( 'feeder-app-setting' );
-			if ( data ) {
-				this.setting = Object.assign( this.setting, JSON.parse( data ) );
+			// ローカルストレージから設定を取得
+			if ( StorageAvailable.has( 'localStorage' ) ) {
+				const data = localStorage.getItem( 'feeder-app-setting' );
+				if ( data ) {
+					state.setting = Object.assign( state.setting, JSON.parse( data ) );
+				}
 			}
-		}
-		this.onAjaxReload();
-	},
+			onAjaxReload();
+		});
 
-	/**
-	 * Vue Destroyed
-	 */
+		onBeforeUnmount( () => {
+			Fix100vh.remove();
+			clearTimeout( RELOAD_TIMER.id );
+		});
+
+		return {
+			state,
+			onBackToTop,
+			onResetSetting,
+			toggleSwitchSetting,
+			changeTab,
+			chengeToastDisplay,
+			changeCategory,
+			onAjaxReload,
+			getQueryAll,
+			debug,
+		};
+	},
+	mounted() {
+		if ( ! this.$options.onScrollEvent ) {
+			return;
+		}
+		requestAnimationFrame( () => {
+			this.$options.onScrollEvent = this.$options.onScrollEvent.bind( this );
+			window.addEventListener( 'scroll', this.$options.onScrollEvent );
+		});
+	},
 	destroyed() {
-		clearTimeout( RELOAD_TIMER.id );
-	},
-
-	/**
-	 * カスタムイベント.
-	 */
-	onScrollEvent() {
-		const windowHeight = window.innerHeight;
-		if ( window.scrollY > windowHeight ) {
-			this.backToTop.display = true;
-		} else {
-			this.backToTop.display = false;
+		if ( ! this.$options.onScrollEvent ) {
+			return;
 		}
+		window.removeEventListener( 'scroll', this.$options.onScrollEvent );
 	},
-
-	/**
-	 * ミックスイン.
-	 */
-	mixins: [ fix100vh, getCurrentClock, isStorageAvailable, onBackToTop, onScrollEvent ],
-
-	/**
-	 * コンフリクト対策.
-	 */
-	delimiters: [ '${', '}' ],
-});
+};
---
+++ public/webpack/src/config.js
@@ -0,0 +1,12 @@
+
+// APIのパス.
+export const API_PATH = '/api/v1/posts';
+
+// 1ページの表示上限.
+export const POSTS_PER_PAGE = 50;
+
+// 自動更新の間隔.
+export const RELOAD_TIMER = {
+	id: 0,
+	interval: 15, // リロード間隔(分)
+};
